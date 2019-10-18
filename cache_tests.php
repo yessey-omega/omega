b2790b61a6137c190d767360ff7282355d24a3fb
yessey87 <mobbing87@yahoo.com>
2019-10-18 10:35:25 +0600

    Initial commit.

---
+++ cache_tests.php
@@ -0,0 +1,4 @@
+    <?php
+	$mem = new Memcached();
+	$mem->addServer("127.0.0.1", 11211);
+    ?>
---
+++ index.php
@@ -0,0 +1,14 @@
+<html> 
+     <head> 
+          <title> 
+              Search
+          </title>
+     </head>
+     <body>
+<form name="f1" method="post" action="search.php">
+<input type="search" name="search"/></br>
+</br>
+<input type="submit" value="поиск"/></br>
+</form>
+    </body>
+</html>
